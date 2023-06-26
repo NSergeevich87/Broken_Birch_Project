722bce3a88d33bb8358d116496d6bf3db1129264
NSergeevich87 <niknikitaser@gmail.com>
2023-06-26 12:50:54 +0200

    Звук и спецэфф.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,17 @@ public class PlayerController : MonoBehaviour
 {
     private Animator playerAnim;
     public bool readyToRun = true;
+    public ParticleSystem explosionParticle;
+    private AudioSource playerAudio;
+    public AudioClip obstacleSound;
+    public AudioClip chickenSound;
     // Start is called before the first frame update
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         playerAnim = GetComponent<Animator>();
         playerAnim.SetBool("Run", true);
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -18,14 +22,19 @@ public class PlayerController : MonoBehaviour
     }
 
 
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAudio.PlayOneShot(obstacleSound, 1.0f);
+            playerAudio.PlayOneShot(chickenSound, 1.0f);
+            explosionParticle.Play();
             playerAnim.SetBool("Eat", true);
             Destroy(collision.gameObject);
             Invoke("eatStop", 0.5f);
             readyToRun = false;
+
         }
     }
 
