4d67035eb5e8f08051f8213c892e600da9c43594
NSergeevich87 <niknikitaser@gmail.com>
2023-08-06 09:40:48 +0200

    SaveManagerScript

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance { get; private set; }
+    //Ниже описываем переменные данные которых будем сохранять
+    private string playerName;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadData();
+    }
+    [System.Serializable]
+    class SaveData
+    {
+        public string name;
+    }
+    public void SaveNewData()
+    {
+        SaveData data = new SaveData();
+        data.name = playerName;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    public void LoadData() 
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path)) 
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            playerName = data.name;
+        }
+    }
+}
